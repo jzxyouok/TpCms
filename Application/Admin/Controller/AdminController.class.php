173ad05208b9c6a3f9a8b2d940913512641be76c
gitrong <547042792@qq.com>
2016-03-02 23:57:32 +0800

    Signed-off-by: gitrong <547042792@qq.com>

---
+++ Application/Admin/Controller/AdminController.class.php
@@ -0,0 +1,15 @@
+<?php
+namespace Admin\Controller;
+
+class AdminController extends CommonController
+{
+    public function index()
+    {
+        $this->display();
+    }
+
+    public function create()
+    {
+        $this->display();
+    }
+}
---
+++ Application/Admin/Controller/ManageController.class.php
@@ -0,0 +1,10 @@
+<?php
+namespace Admin\Controller;
+
+class ManageController extends CommonController
+{
+    public function index()
+    {
+        $this->display();
+    }
+}
