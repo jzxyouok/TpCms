fea05916619074e459f0af165307ec51ae52fad2
gitrong <547042792@qq.com>
2016-03-18 01:08:44 +0800

    Signed-off-by: gitrong <547042792@qq.com>

--- Application/Admin/Controller/ArticleController.class.php
+++ Application/Admin/Controller/ArticleController.class.php
@@ -17,7 +17,24 @@ class ArticleController extends CommonController
     public function create()
     {
         if(IS_POST && !IS_AJAX){
+            $data['category_id'] = I('param.category_id');
+            $data['title'] = I('param.title');
+            $data['content'] = I('param.content');
+            $data['description'] = I('param.description');
+            $data['visible'] = I('param.visible');
+            $data['due_at'] = I('param.due_at');
+            $data['priority'] = I('param.priority');
+            $data['browse'] = I('param.browse');
 
+            $article = D('Article');
+
+            $data = $article->create($data);
+
+            if($data && $article->add($data)){
+                $this->success('操作完成','/admin/article',1);
+            }else{
+                $this->error('操作失败'.$article->getError());
+            }
         }else{
             $category = new CategoryController();
 
--- Application/Admin/Model/ArticleModel.class.php
+++ Application/Admin/Model/ArticleModel.class.php
@@ -4,10 +4,21 @@ use Think\Model;
 class ArticleModel extends Model {
 
     // 自动验证
-    protected $_validate = [];
+    protected $_validate = [
+        ['title','require','文章标题必填'],
+        ['title','','文章标题已经存在',0,'unique',3],
+        ['content','require','文章内容必填'],
+        ['content','200,30000','文章内容必须200-30000字内',0,'length'],
+        ['description','require','文章描述必填'],
+        ['priority','/^[0-9]+$/','优先级必须整数',0,'regex'],
+        ['browse','/^[0-9]+$/','浏览量必须整数',0,'regex'],
+    ];
 
     // 自动完成
-    protected $_auto = [];
+    protected $_auto = [
+        ['created_at','time',1,'function'],
+        ['updated_at','time',2,'function'],
+    ];
 
     // 插入数据前的回调方法
     protected function _before_insert(&$data, $options)
